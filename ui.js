6a04af08ac1b135e75cec6816c44a1113503df6d
mazmulj <55931553+mazmulj@users.noreply.github.com>
2020-05-15 05:30:30 -0230

    Add files via upload

--- ui.js
+++ ui.js
@@ -4,6 +4,18 @@ class UI{
     }
 
     showProfile(user){
-        
+        this.profile.innerHTML = `
+            <div class = "card card-body mb-3">
+                <div class = "row">
+                    <div class = "col-md-3">
+                        <img class="img-fluid mb-2" src="${user.avatar_url}">
+                        <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block">View User</a> 
+                    </div>
+                    <div class = "col-md-9">
+                        
+                    </div>
+                </div>
+            </div>
+        `;
     }
 }
