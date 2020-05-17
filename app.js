6324ae3d6b367593a2eddfe049cb116ff53905c5
mazmulj <55931553+mazmulj@users.noreply.github.com>
2020-05-16 23:28:30 -0230

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// INIT github and ui classes
+const github = new GitHub;
+const ui = new UI;
+
 //Search Username
 const searchUser = document.getElementById('searchUser');
 
@@ -5,6 +9,17 @@ searchUser.addEventListener('keyup', (e) => {
     const userText = e.target.value;
 
     if (userText !== ''){
+        github.getUser(userText)
+        .then(data => {
+            if(data.profile.message === 'Not Found')
+            {
 
+            } else {
+                ui.showProfile(data.profile);
+
+            }
+        })
+    } else{
+        ui.clearProfile();
     }
 });
