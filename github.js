ff164f92cf7a4ef137fa0e620d1cd519eea4171f
mazmulj <55931553+mazmulj@users.noreply.github.com>
2020-06-23 05:03:11 -0230

    Add files via upload

--- github.js
+++ github.js
@@ -5,7 +5,10 @@ class GitHub{
     }
 
     async getUser(user){
-        const profileResponse = await fetch(`hhtp://api.github.com/users/${users}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
 
         const profile = await profileResponse.json();
 
