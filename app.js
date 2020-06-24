45caa589386dea7cc12934bf37a0f83dd8e261ff
mazmulj <55931553+mazmulj@users.noreply.github.com>
2020-06-24 00:09:17 -0230

    Add files via upload

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ searchUser.addEventListener('keyup', (e) => {
         .then(data => {
             if(data.profile.message === 'Not Found')
             {
-
+                ui.showAlert('User not found', 'alert alert-danger');
             } else {
                 ui.showProfile(data.profile);
-
+                ui.showRepos(data.repos);
             }
         })
     } else{
